36936b6760023f25bb0ea54957dfadec9e413060
johnson ma <mfdboy@163.com>
2017-06-15 15:57:06 +0800

    update mfd

---
+++ src/local/johnson/finalutils/event/datamerge/Choose.java
@@ -0,0 +1,28 @@
+package local.johnson.finalutils.event.datamerge;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JFileChooser;
+
+import local.johnson.finalutils.panel.DataMerge;
+
+public class Choose implements ActionListener {
+	
+	public DataMerge dataMerge;
+
+	public Choose(DataMerge dataMerge) {
+		this.dataMerge = dataMerge;
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		dataMerge.chooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+		dataMerge.chooser.setDialogTitle("select a directory");
+		int returnVal = dataMerge.chooser.showOpenDialog(dataMerge.chooser);
+		if (returnVal == JFileChooser.OPEN_DIALOG) {
+			dataMerge.input.setText(dataMerge.chooser.getSelectedFile().getAbsolutePath());
+		}
+	}
+
+}
--- src/local/johnson/finalutils/panel/DataMerge.java
+++ src/local/johnson/finalutils/panel/DataMerge.java
@@ -1,7 +1,15 @@
 package local.johnson.finalutils.panel;
 
+import java.awt.FlowLayout;
 import java.awt.LayoutManager;
+
+import javax.swing.JButton;
+import javax.swing.JFileChooser;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.JTextPane;
+import local.johnson.finalutils.event.datamerge.Choose;
 
 public class DataMerge extends JPanel {
 
@@ -9,9 +17,29 @@ public class DataMerge extends JPanel {
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
+	
+	public JButton chooseButton = new JButton();
+
+	public JButton processButton = new JButton();
+
+	public JTextField input = new JTextField(15);
+
+	public JLabel label = new JLabel("File:");
+
+	public JFileChooser chooser = new JFileChooser();
+
+	public JTextPane msg = new JTextPane();
 
 	public DataMerge() {
-		// TODO Auto-generated constructor stub
+		chooseButton.setText("choose");
+		chooseButton.addActionListener(new Choose(this));
+		processButton.setText("process");
+		//processButton.addActionListener(new Process(this));
+		add(label);
+		add(input);
+		add(chooseButton);
+		add(processButton);
+		setLayout(new FlowLayout());
 	}
 
 	public DataMerge(LayoutManager layout) {
